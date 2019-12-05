b0ef8040458aabaf57b596fcc8b27d049cff600a
Jeffrey Berube <berubejd@gmail.com>
2019-12-05 16:05:52 -0600

    Completed Bite 55 Get the latest game releases from Steam's RSS feed.  Ends up they wanted the item title and link and nothing from the actual game that was in the HTML.

--- 55/steam.py
+++ 55/steam.py
@@ -2,10 +2,7 @@
 
 from collections import namedtuple
 
-from bs4 import BeautifulSoup
 import feedparser
-
-from pprint import pp
 
 # cached version to have predictable results for testing
 FEED_URL = "http://bit.ly/2IkFe9B"
@@ -15,16 +12,7 @@ Game = namedtuple("Game", "title link")
 
 def get_games():
     """Parses Steam's RSS feed and returns a list of Game namedtuples"""
-    
-    game_list = []
-    
-    rss_data = feedparser.parse(FEED_URL)
-    
-    for entry in rss_data.entries:
-        soup = BeautifulSoup(entry.summary, "html.parser")
-        link = soup.find("a")
-        game_list.append(Game(link.text, link["href"]))
 
-    return game_list
+    return [Game(entry.title, entry.link) for entry in feedparser.parse(FEED_URL).entries]
 
 print(get_games())
