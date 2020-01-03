5d518dcdb6b0f82d1767cd425ff67f4dfd0f162a
Jeffrey Berube <berubejd@gmail.com>
2020-01-03 17:47:29 -0600

    Forgot final save or something...

--- 74/bday.py
+++ 74/bday.py
@@ -5,18 +5,13 @@ import calendar
 
 def weekday_of_birth_date(date):
     """Takes a date object and returns the corresponding weekday string"""
-    
-    
     return calendar.day_name[calendar.weekday(date.year, date.month, date.day)]
-
-
 
 
 from datetime import date
 
 dt = date(1974, 11, 11)
 print(weekday_of_birth_date(dt)) # 'Monday'
-print(str(dt))
 
 """
 Bite 74. What day of the week were you born on? ☆
