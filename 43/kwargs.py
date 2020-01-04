3a7fe9489806704debe55cbb1e4ef3172510997b
Jeffrey Berube <berubejd@gmail.com>
2020-01-03 18:09:10 -0600

    Completed Bite 43 Force keyword arguments

---
+++ 43/kwargs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.8
+
+def get_profile(*, name="julian", profession="programmer"):
+    return f"{name} is a {profession}"
+
+print(get_profile())
+
+"""
+Bite 43. Force keyword arguments ☆
+Write a function called get_profile that can only allows 2 keyword arguments: name and profession which default to julian and programmer respectively.
+
+The function does nothing fancy, just return a str: name is a profession.
+
+The point is to limit the interface of this function and you'll see Python makes it very easy ... have fun!
+"""
