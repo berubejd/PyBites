8631805ed47ba415a5c5b9181bd30b5113da23f0
Jeffrey Berube <berubejd@gmail.com>
2020-08-29 03:04:00 -0500

    Completed Bite 56 Add a commandline interface to our BMI calculator

---
+++ 56/bmi.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3.8
+
+import argparse
+
+
+def calc_bmi(weight, length):
+    """Provided/DONE:
+       Calc BMI give a weight in kg and length in cm, return the BMI
+       rounded on 2 decimals"""
+    bmi = int(weight) / ((int(length) / 100) ** 2)
+    return round(bmi, 2)
+
+
+def create_parser():
+    """
+    $ python bmi.py -h
+    usage: bmi.py [-h] [-w WEIGHT] [-l LENGTH]
+
+    Calculate your BMI.
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -w WEIGHT, --weight WEIGHT
+                            Your weight in kg
+      -l LENGTH, --length LENGTH
+                            Your length in cm
+
+    """
+
+    parser = argparse.ArgumentParser(description="Calculate your BMI.")
+
+    parser.add_argument(
+        "-w", "--weight", help="Your weight in kg", action="store", type=int,
+    )
+
+    parser.add_argument(
+        "-l", "--length", help="Your length in cm", action="store", type=int,
+    )
+
+    return parser
+
+
+def handle_args(args=None):
+    """Provided/DONE:
+       Call calc_bmi with provided args object.
+       If args are not provided get them from create_parser"""
+    if args is None:
+        parser = create_parser()
+        args = parser.parse_args()
+
+    if args.weight and args.length:
+        bmi = calc_bmi(args.weight, args.length)
+        print(f"Your BMI is: {bmi}")
+    else:
+        # could enforce SystemExit in create_parser/add_argument, but argparse
+        # docs are not clear how to do it, so raising the exception here manually
+        raise SystemExit
+
+
+if __name__ == "__main__":
+    handle_args()
+
