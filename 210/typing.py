ff0a2be12d12560c9e02af4960753d979123d2a4
Jeffrey Berube <berubejd@gmail.com>
2020-09-19 00:11:01 -0500

    Completed Bite 210 Add Type Annotations

---
+++ 210/typing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.8
+from typing import List
+
+
+def sum_numbers(numbers: List[int]) -> int:
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
