84b6fcaaa32808b2036b4a46535fdcca478cdd07
Jeffrey Berube <berubejd@gmail.com>
2020-09-19 00:04:59 -0500

    Completed Bite 209 Write a Spinx docstring

---
+++ 209/sphinx.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.8
+"""
+In this Bite you complete find_number_pairs which receives a list of 
+numbers and returns all the pairs that sum up to N (default=10). Return 
+this list of tuples from the function.
+
+So in the most basic example if we pass in [2, 3, 5, 4, 6] it 
+returns [(4, 6)] and if we give it [9, 1, 3, 8, 7] it returns 
+[(9, 1), (3, 7)]. The tests check more scenarios (floats, other values 
+of N, negative numbers).
+"""
+
+
+def sum_numbers(numbers):
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+
+    :raises TypeError: if not all numeric values passed in
+
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
